6b460c5473a34996df26b4ba56f0f8a9341e64ee
Matt Martin <matthewjmartin@zoho.com>
2021-05-06 07:04:23 +0100

    initial backend setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e6f518dac31a4e3bfd3db002cf716ef4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fa843910-8071-4fac-a489-99fe5febb0de'
+};
